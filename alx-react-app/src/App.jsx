02d835e35aa117b6a451535f549e9a51c5ed313e
MaggieNush <maggienungari.mn@gmail.com>
2025-07-08 12:06:40 +0300

    Add user profile in the react app

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -6,6 +6,7 @@ import WelcomeMessage from './components/WelcomeMessage'
 import Header from './components/Header'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
+import UserProfile from './components/UserProfile'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -16,8 +17,9 @@ function App() {
         <WelcomeMessage />
         <Header />
         <MainContent />
+        <UserProfile name="Alice" age="25" bio="Loves hiking and photography"/>
         <Footer />
-        <a href="https://vite.dev" target="_blank">
+        {/* <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
@@ -35,7 +37,8 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
+      </div>
     </>
   )
 }
