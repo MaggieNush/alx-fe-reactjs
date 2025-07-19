79943e0f54bd9ce318ce89a3a40274d8a209d0ec
MaggieNush <maggienungari.mn@gmail.com>
2025-07-19 11:12:27 +0300

    Add a new Counter component

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -7,6 +7,7 @@ import Header from './components/Header'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
 import UserProfile from './components/UserProfile'
+import Counter from './components/Counter'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -14,11 +15,12 @@ function App() {
   return (
     <>
       <div>
-        <WelcomeMessage />
-        <Header />
-        <MainContent />
+        {/* <WelcomeMessage />
+        <Header /> */}
+        <Counter />
+        {/* <MainContent />
         <UserProfile name="Alice" age="25" bio="Loves hiking and photography"/>
-        <Footer />
+        <Footer /> */}
       </div>
     </>
   )
