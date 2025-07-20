5a1fc3bc872695cf6dedbc594254202d658b9462
MaggieNush <maggienungari.mn@gmail.com>
2025-07-20 23:10:20 +0300

    Add a new react props folder

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,7 +1,11 @@
-import UserInfo from '../UserInfo';
+const UserProfile = (props) => {
+    return (
+        <div style={{border:'2px solid gray', padding:'10px', margin:'10px'}}>
+            <h2 style={{color:'skyblue', fontStyle:'italic', fontSize:'40px'}}>{props.name}</h2>
+            <p>Age: <span style={{fontWeight:'bold'}}> {props.age}</span></p>
+            <p>Bio: <span style={{fontStyle:'italic', fontWeight:'bold'}}> {props.bio}</span></p>
+        </div>
+    );
+};
 
-function UserProfile() {  
-  return <UserInfo />;
-}
-
-export default UserProfile;  
+export default UserProfile;
