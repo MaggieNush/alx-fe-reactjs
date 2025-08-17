7ab9785f5c33d8d94e3aa634a88d18dea4418b3c
MaggieNush <maggienungari.mn@gmail.com>
2025-08-17 20:14:24 +0300

    Build the recipe detail page

--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -1,11 +1,9 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+ /** @type {import('tailwindcss').Config} */
+export default {
+   content: ["./public/index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+   darkMode: false,
+   theme: {
+     extend: {},
+   },
+   plugins: [],
+ };
